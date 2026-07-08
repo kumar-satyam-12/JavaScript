d9901cfdcb5cfc79531334a066332c646d3db7e5
Satyam <sk950400@gmail.com>
2026-07-08 15:09:57 +0530

    new

--- JavaScript/20_Classes_and_OOP/Object_Literal.js
+++ JavaScript/20_Classes_and_OOP/Object_Literal.js
@@ -20,7 +20,7 @@ console.log(this) // now here in node environment in our pc this is for global c
 
 //here user is an object
 
-function hello(username, sex, state){
+function hello(username, sex, state){  // hello is a constructor function
     this.username = username
     this.sex = sex
     this.state = state
@@ -28,9 +28,11 @@ function hello(username, sex, state){
 }
 
 const one = new hello("maytas", 'M', "MP" )
+const two = new hello("rumak", "M", "GJ")
 console.log(one)
 
 // new is a JavaScript operator that is used to call a constructor function or a class constructor to create a new object.
+//new creates a new object whose properties are initialized by the constructor function
 // that means one is an object with properties username, sex and state
 /*We use the constructor function hello to initialize the properties of a newly created object.
  The new operator creates the object and then calls hello to initialize it. */
---
+++ JavaScript/20_Classes_and_OOP/Prototype.js
@@ -0,0 +1,61 @@
+//Protoype
+
+/*  A prototype is a shared object that stores methods and properties so that all objects created
+ from the same constructor can inherit and reuse them instead of having their own copies
+
+function hello(username){
+    this.username = username;
+
+    this.greet = function(){
+    console.log(this.username)
+    }
+   
+
+}
+
+const u1 = new hello("satyam")
+const u2 = new hello("Rahul")
+
+
+
+
+like here both objects u1 and u2 have their own copy of hello constructor (which is inefficient), so this issue will be resolved using the prototype
+
+ */
+
+function username(name, age){
+  this.name = name;
+  this.age = 12;
+
+}
+
+username.prototype.greet = function(){ // greet is a method that is stopped inside prototype object
+    console.log(this.name)  //username.prototype is the prototype object
+}
+username.prototype.agefuture = function(){
+    console.log(`age of ${this.name} after 10 yrs would be ${(this.age)+10}`)
+}
+
+/*
+We are creating a function inside the prototype of username named
+ greet, so whichever object gets new username also gets access to the prototype.
+ */
+
+
+
+
+const a1 = new username ("Satyam", 23)
+const a2 = new username("Rahul", 35)
+
+a1.agefuture()
+a2.agefuture()
+
+
+//how JS finds greet when we write a1.greet()?
+//JS checks whether a1 has greet property? No
+// JS looks in u1 prototype and finds username.prototype and executes the function
+
+// now here both the objects a1 and a2 are sharing the same greet method using prototype
+
+/*The prototype is mainly used to store methods so that all objects created from the same constructor
+ can share those methods instead of each object having its own copy. */
